d9fcadd35cb5dfeee78310565df4aff37a80ded2
zhan <zjc_9999@126.com>
2019-02-26 15:40:25 +0800

    test

--- MyJavaTest/src/HelloWorld.java
+++ MyJavaTest/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("This is a Git check-out project.");
 		System.out.println("中文：中文测试。");
+		//测试下vscode中的git同步功能
 	}
 
 }
