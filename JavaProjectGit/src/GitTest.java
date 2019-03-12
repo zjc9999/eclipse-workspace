d035006307b73afaf4f9afe166af31cb62c9b214
zjc9999 <zjc_9999@126.com>
2019-03-12 13:53:43 +0800

    SettingSync Worked.

--- JavaProjectGit/src/GitTest.java
+++ JavaProjectGit/src/GitTest.java
@@ -2,6 +2,7 @@
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("JavaProjectGit test.");
+		System.out.println("2019-3-12 It's worked of SettingSync.");
 	}
 
 }
